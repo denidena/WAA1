96037cf6ae3f4f810c9487f91a8382128c3595f2
denidena <denisa.pucikova@gmail.com>
2019-04-29 19:48:52 +0200

    vynatie tlacidla do samostatnej premennej

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -1,86 +1,91 @@
 package tests;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 //komentar
 public class GosslingatorTest {
 
-    @Test
-    public void itShouldAddOneRyan() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+    WebDriver driver;
 
+    @Before
+    public void setUp() {
+        //v ramci triedy a vytvorenie metody - úvod
+        //spustenie prehliadaca, otvorenie stranky,
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         //0. spustit prehliadac
         //co znamena ten zapis => vytvaram novu instanciu , chcem vytvorit novy objekt "driver" z triedy WebDriver
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         //1. otvorit stranku
         driver.get("http://localhost:80/gosslingator.php");
+    }
+
+    @After
+    public void tearDown() {
+        //vytvorenie záveru ktory sa sam spusta vzdy
+        //4.zatvorit prehliadac
+        driver.close();
+        //5. ukoncit session
+        driver.quit();
+        System.out.println("koncim...");
+    }
+
+
+    @Test
+    public void itShouldAddOneRyan() {
+        //vytvorenie elementu a najdenie iba raz ale pouzitie viackrat
+        WebElement addRyanBtn = driver.findElement(By.id("addRyan"));
         //2. kliknut na tlacitko pridat
-        driver.findElement(By.id("addRyan")).click();
-        //driver.findElement(By.id("addRyan")).click();
-        //driver.findElement(By.id("removeRyan")).click();
+        addRyanBtn.click();
+        //addRyanBtn.click();
+        //addRyanBtn.click();
         // 3. overit pocitanie ryanov
         Assert.assertEquals("1", driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
         Assert.assertEquals("ryan", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
         // vypisem do konzoly pocitadlo ryanov
         System.out.println("Number of Ryans is: " + driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
-        //4.zatvorit prehliadac
-        driver.close();
-        //5. ukoncit session
-        driver.quit();
         // driver.findElement(By.id("ryanCounter")).getText();
-
     }
 
     @Test
     public void itShouldAddTwoRyans() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-
-        //0. spustit prehliadac
-        //co znamena ten zapis => vytvaram novu instanciu , chcem vytvorit novy objekt "driver" z triedy WebDriver
-        WebDriver driver = new ChromeDriver();
-        //1. otvorit stranku
-        driver.get("http://localhost:80/gosslingator.php");
         //2. kliknut na tlacitko pridat
         //for (int i = 0; i < 51; i++)
-
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("removeRyan")).click();
+        //definovanie webelementov
+        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
+        WebElement removeRyanButton = driver.findElement(By.id("removeRyan"));
+        //alt+J oznacenie kazdeho dalsieho  vyskytu
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        removeRyanButton.click();
         // 3. overit pocitanie ryanov
-        Assert.assertEquals("2", driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
-        Assert.assertEquals("ryans", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
-        System.out.println("Number of Ryans is: " + driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
-        //4.zatvorit prehliadac
-        driver.close();
-        //5. ukoncit session
-        driver.quit();
-        // driver.findElement(By.id("ryanCounter")).getText();
-
+        String NumberOfRyan = "div.ryan-counter ";
+        String actualNumberOfRyans = driver.findElement(By.cssSelector(NumberOfRyan +"h2")).getText();
+        String actualRyanDesc = driver.findElement(By.cssSelector(NumberOfRyan +"h3")).getText();
+        Assert.assertEquals("2",actualNumberOfRyans);
+        Assert.assertEquals("ryans", actualRyanDesc);
+        System.out.println("Number of Ryans is: " + actualNumberOfRyans);
     }
 
     @Test
     public void itShouldDisplayTitle() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:80/gosslingator.php");
         System.out.println(driver.findElement(By.cssSelector(".ryan-title")).getText());
         Assert.assertEquals("GOSLINGATE ME", driver.findElement(By.cssSelector(".ryan-title")).getText());
-        driver.close();
-        driver.quit();
+
     }
 
     @Test
     public void itShouldAdd50RyansAndShowMessage() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:80/gosslingator.php");
+        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
         for (int i = 0; i < 50; i++) {
-            driver.findElement(By.id("addRyan")).click();
+            addRyanButton.click();
         }
         Assert.assertEquals("50", driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
         Assert.assertEquals("ryans", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
@@ -89,8 +94,6 @@ public class GosslingatorTest {
                 "RYANS\n" +
                 "IS TOO DAMN\n" +
                 "HIGH", driver.findElement(By.cssSelector("h1.tooManyRyans")).getText());
-        driver.close();
-        driver.quit();
 
     }
 
