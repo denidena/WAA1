cf2772c40b27019775b3713973433b9e2dee1472
denidena <denisa.pucikova@gmail.com>
2019-05-22 19:21:23 +0200

    Pokemon page - spravenie paegu a refaktorizacia kodu, vynatie metod do pageu.

---
+++ src/test/java/pages/PokePokePage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class PokePokePage {
+
+    private WebDriver pageDriver;
+
+    public PokePokePage(WebDriver driver) {
+        this.pageDriver = driver;
+    }
+
+    public void selectPokemon(String pokemonToSelect) {
+        WebElement pokemonSelect = pageDriver.findElement(By.cssSelector("select"));
+        new Select(pokemonSelect).selectByVisibleText(pokemonToSelect);
+    }
+
+    public String getActualMessage() {
+        return pageDriver.findElement(By.cssSelector("div.pokemon h3")).getText();
+    }
+
+    public String getExpectedMessage(String pokemonName) {
+        return String.format("I choose you %s !", pokemonName);
+    }
+}
--- src/test/java/testFurbyCheck/PokemonTestik.java
+++ src/test/java/testFurbyCheck/PokemonTestik.java
@@ -1,20 +1,20 @@
 package testFurbyCheck;
 
+import base.TestBase;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-
-import base.TestBase;
+import pages.PokePokePage;
 
 public class PokemonTestik extends TestBase {
+
+    private PokePokePage PokePokePage;
 
     @Before
     public void openPage() {
         //1.otvorit stranku
         driver.get(BASE_URL + "/vybersi.php");
+        PokePokePage = new PokePokePage(driver);
     }
 
     @Test
@@ -23,23 +23,11 @@ public class PokemonTestik extends TestBase {
 
         for (String pokemon : selectedPokemons) {
             //vyberiem pokemona
-            selectPokemon(pokemon);
+            PokePokePage.selectPokemon(pokemon);
             //overim hlasku
-            Assert.assertEquals(getExpectedMessage(pokemon), getActualMessage());
+            Assert.assertEquals(PokePokePage.getExpectedMessage(pokemon), PokePokePage.getActualMessage());
         }
     }
 
-    private void selectPokemon(String pokemonToSelect) {
-        WebElement pokemonSelect = driver.findElement(By.cssSelector("select"));
-        new Select(pokemonSelect).selectByVisibleText(pokemonToSelect);
-    }
-
-    private String getActualMessage() {
-        return driver.findElement(By.cssSelector("div.pokemon h3")).getText();
-    }
-
-    private String getExpectedMessage(String pokemonName) {
-        return String.format("I choose you %s !", pokemonName);
-    }
 
 }
