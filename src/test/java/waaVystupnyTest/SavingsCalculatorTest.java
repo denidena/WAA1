76c20c67178c06ab5b5cf3a478bd91fbf22a8634
denidena <denisa.pucikova@gmail.com>
2019-06-03 18:50:06 +0200

    test01 a test02 je up-and-running ale je velmi skaredy tiez

--- src/test/java/waaVystupnyTest/SavingsCalculatorTest.java
+++ src/test/java/waaVystupnyTest/SavingsCalculatorTest.java
@@ -34,14 +34,16 @@ public class SavingsCalculatorTest {
 
 
     @Test
-    public void test01ApplyForSavingsIsEnabled()
-    {
+    public void test01ApplyForSavingsIsEnabled() {
+        // zadam vsetky informacie(fond, suma, email, roky)
+        // tlacidlo 'Apply for saving' je enabled
+
         System.out.println("test1...");
+
         //insert at first the fixed variables
-        // Test01 = zadam vsetky informacie(fond, suma, email, roky)
         String inputFund = "Tom & Jerry corp";
         String inputSum = "1000";
-        String inputEmail= "1@tralala.com";
+        String inputEmail = "1@tralala.com";
         String inputYears = "15";
 
         // fill in the fixed values
@@ -53,26 +55,74 @@ public class SavingsCalculatorTest {
         // check if the button is enabled by cssSelector
         //expected action is that class when enabled "btn btn-block btn-success"
 
-        //driver.findElement(By.cssSelector("button.btn-success")).isEnabled();
-        //Assert.assertEquals("btn btn-block btn-success", driver.findElement(By.cssSelector("button.btn-success")).getAttribute("class"));
+        // V1
+        // Assert.assertEquals("btn btn-block btn-success", driver.findElement(By.cssSelector("button.btn-success")).getAttribute("class"));
+        // V2
         Assert.assertTrue(driver.findElement(By.cssSelector("button.btn-success")).isEnabled());
     }
 
     @Test
-    public void test02CheckIfSumsAreNotEmpty()
-    {
+    public void test02CheckIfSumsAreNotEmpty() {
+
+        // zadam vsetky informacie(fond, suma, email, roky)
+        // overim ze sumy nie su prazdne
+
         System.out.println("test2...");
+
+        //insert at first the fixed variables
+        String inputFund = "Tom & Jerry corp";
+        String inputSum = "1000";
+        String inputEmail = "1@tralala.com";
+        String inputYears = "15";
+
+        // fill in the fixed values
+        driver.findElement(By.id("fundSelect")).sendKeys(inputFund);
+        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys(inputSum);
+        driver.findElement(By.id("yearsInput")).sendKeys(inputYears);
+        driver.findElement(By.id("emailInput")).sendKeys(inputEmail);
+
+        //.div[1]/p" div.result matove riesenie
+        // a som na to prisla presne o 18:40  (//body//div//div//div//p)[1]
+        //driver.findElement(By.xpath("(//body//div//div//div//p)[1]")).getText();
+        //By.xpath("(//body//div//div//div//p)[1]"), By.xpath("(//body//div//div//div//p)[2]")
+
+        //expected action is that sums are not empty
+        Assert.assertNotEquals("", driver.findElement(By.xpath("(//body//div//div//div//p)[1]")).getText());
+        Assert.assertNotEquals("", driver.findElement(By.xpath("(//body//div//div//div//p)[2]")).getText());
+
     }
 
     @Test
-    public void test03CheckIfRiskIsNotEmpty()
-    {
+    public void test03CheckIfRiskIsNotEmpty() {
+        // zadam vsetky informacie(fond, suma, email, roky)
+        // overim ze riziko nie je prazdne
+
         System.out.println("test3...");
+
+        //insert at first the fixed variables
+        String inputFund = "Tom & Jerry corp";
+        String inputSum = "1000";
+        String inputEmail = "1@tralala.com";
+        String inputYears = "15";
+
+        // fill in the fixed values
+        driver.findElement(By.id("fundSelect")).sendKeys(inputFund);
+        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys(inputSum);
+        driver.findElement(By.id("yearsInput")).sendKeys(inputYears);
+        driver.findElement(By.id("emailInput")).sendKeys(inputEmail);
+
+        //.div[1]/p" div.result matove riesenie
+        // a som na to prisla presne o 18:40  (//body//div//div//div//p)[1]
+        //driver.findElement(By.xpath("(//body//div//div//div//p)[1]")).getText();
+        //By.xpath("(//body//div//div//div//p)[1]"), By.xpath("(//body//div//div//div//p)[2]")
+
+        //expected action is that sums are not empty
+        Assert.assertNotEquals("", driver.findElement(By.xpath("(//body//div//div//div//p)[3]")).getText());
+
     }
 
     @Test
-    public void test04CheckIfRecentRequestContainsFund()
-    {
+    public void test04CheckIfRecentRequestContainsFund() {
         System.out.println("test4...");
     }
 
